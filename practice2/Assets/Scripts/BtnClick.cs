b15b6c0e7e761686148671ac44a1294d06575055
liuzeliang <434421502@qq.com>
2018-12-11 11:56:27 +0800

    lzl
    0000 first version

---
+++ practice2/Assets/Scripts/BtnClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+public Entity e;
+    public void showText(){
+		Debug.Log("FFFFFFF");
+		var tg = GameObject.Find("Global").GetComponent<TestGlobal>();
+		tg.TestFunc();
+        var textFile = Resources.Load<TextAsset>("test");
+        var data = JsonUtility.FromJson<TestData>(textFile.text);
+		Debug.Log(data.a);
+		var textCom = GameObject.Find("Canvas/TextName").GetComponent<Text>();
+		float num = Random.value;
+		textCom.text = "测试名字" + num.ToString();
+		string name = "测试1";
+		int age = 1;
+		string sex = "男";
+		// Vector3 v3;
+		// v3.Set(0.0, 0.0, 0.0);
+		Entity entity = Instantiate(e, new Vector3(0, 0, 0),Quaternion.identity);
+        entity.Add( name, age, sex);
+	}
+
+	public void Click()
+	{
+		Debug.Log("FUCK___");
+	}
+}
---
+++ practice2/Assets/Scripts/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+	string sex;
+	int age;
+	string entityEame = "";
+	// Use this for initialization
+	void Start () {
+		Debug.Log("名字" + this.entityEame);
+		Debug.Log("性别" + this.sex);
+		Debug.Log("年龄" + this.age);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Add( string name, int age, string sex)
+	{
+		this.entityEame = name;
+		this.age = age;
+		this.sex = sex;
+	}
+}
